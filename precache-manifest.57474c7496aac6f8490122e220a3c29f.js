a5342abf6499322925d2741f8fd7c4bd8c6880ea
Supakoa <41123305+Supakoa@users.noreply.github.com>
2019-05-08 20:48:09 +0700

    Updates

---
+++ precache-manifest.57474c7496aac6f8490122e220a3c29f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d266d6b507d60a8ad7d7eed06c1ca8ca",
+    "url": "/ce-app/index.html"
+  },
+  {
+    "revision": "60d36f92a2f9b2fadd17",
+    "url": "/ce-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2ab160f2be824052bbda",
+    "url": "/ce-app/static/css/main.a58be3e4.chunk.css"
+  },
+  {
+    "revision": "60d36f92a2f9b2fadd17",
+    "url": "/ce-app/static/js/2.15c8f666.chunk.js"
+  },
+  {
+    "revision": "2ab160f2be824052bbda",
+    "url": "/ce-app/static/js/main.fc09a13f.chunk.js"
+  },
+  {
+    "revision": "da714c47ea02db977557",
+    "url": "/ce-app/static/js/runtime~main.c4f1adbb.js"
+  },
+  {
+    "revision": "a32aa95c567b8520cba2b95a77f9db55",
+    "url": "/ce-app/static/media/banner.a32aa95c.jpg"
+  }
+]);
